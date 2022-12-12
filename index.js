3c5e610321fee06c48cc5458981151acbc716437
chicoedumacedo <chicoedumacedo@gmail.com>
2022-12-12 10:39:03 -0300

    primeiro js

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("hello word");
+console.log(5 + 8);
+console.log(4 + 6);
+
+let nome = "Eduardo Macedo";
+const dataNascimento = "09/07/1981";
+
+
+document.write("<h1>Hello Word</h1>");
+document.write(`<h2>Nascido em: ${dataNascimento}</h2>`);
+document.write(nome);
+document.write("<br>")
+nome = "Duda";
+document.write(nome);
+document.getElementsByTagName("h1")[0].style.color = "red";
+document.write(dataNascimento);
+
+
